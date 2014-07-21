de627d02f0bb8430711f1e5513e9dc40a04e6246
Ryan Dew <ryan.j.dew@gmail.com>
2014-07-20 22:34:40 -0600

    Add inital code for structured queries

--- query.go
+++ query.go
@@ -1,6 +1,56 @@
 package go_marklogic_go
 
 import (
-//"encoding/json"
+//"encoding/xml"
 //"io"
 )
+
+// Query represents a structured query from the search API
+type Query struct {
+	XMLName xml.Name      `xml:"http://marklogic.com/appservices/search query"`
+	Queries []interface{} `xml:",any"`
+}
+
+type OrQuery struct {
+	XMLName xml.Name      `xml:"http://marklogic.com/appservices/search or-query"`
+	Queries []interface{} `xml:",any"`
+}
+
+type AndQuery struct {
+	XMLName xml.Name      `xml:"http://marklogic.com/appservices/search and-query"`
+	Queries []interface{} `xml:",any"`
+	Ordered bool          `xml:"http://marklogic.com/appservices/search ordered"`
+}
+
+type TermQuery struct {
+	XMLName xml.Name  `xml:"http://marklogic.com/appservices/search term-query"`
+	Terms   []*string `xml:"http://marklogic.com/appservices/search text"`
+	Weight  float64   `xml:"http://marklogic.com/appservices/search weight"`
+}
+
+type AndNotQuery struct {
+	XMLName       xml.Name      `xml:"http://marklogic.com/appservices/search and-not-query"`
+	PositiveQuery PositiveQuery `xml:"http://marklogic.com/appservices/search positive-query"`
+	NegativeQuery NegativeQuery `xml:"http://marklogic.com/appservices/search negative-query"`
+}
+
+type PositiveQuery struct {
+	XMLName xml.Name      `xml:"http://marklogic.com/appservices/search positive-query"`
+	Queries []interface{} `xml:",any"`
+}
+
+type NegativeQuery struct {
+	XMLName xml.Name      `xml:"http://marklogic.com/appservices/search negative-query"`
+	Queries []interface{} `xml:",any"`
+}
+
+type NotQuery struct {
+	XMLName xml.Name      `xml:"http://marklogic.com/appservices/search not-query"`
+	Queries []interface{} `xml:",any"`
+}
+
+type NotInQuery struct {
+	XMLName       xml.Name      `xml:"http://marklogic.com/appservices/search not-in-query"`
+	PositiveQuery PositiveQuery `xml:"http://marklogic.com/appservices/search positive-query"`
+	NegativeQuery NegativeQuery `xml:"http://marklogic.com/appservices/search negative-query"`
+}
